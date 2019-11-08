521afdc4500c6ab801e06571f7dda9e7355ebf87
Max Kullmann <max@eduroam-056-171.scc.uni-weimar.de>
2019-11-08 16:56:23 +0100

    Factorial exercise solved

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 
 public class Factorial {
 	//todo: implement a recursive functions whicht gives you the factorial value of a given number
-	//Test test test
+
+	public static void main (String[] args){
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		System.out.println(fac(n));
+
+	}
+
+	public static int fac (int n){
+
+		int result;
+
+		if(n < 1){
+			
+			return 1;
+
+		}else {
+
+			result = n * fac(n-1);
+
+			return result;
+		
+		}		
+
+	}
+
 }
